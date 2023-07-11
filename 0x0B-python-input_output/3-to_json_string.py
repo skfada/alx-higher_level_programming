1f31016be7cc1e2cb63365b7daf3be475e13e3d4
skfada <adamusolomon44@gmail.com>
2023-07-11 19:01:14 +0100

    3-to_json_string.py

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+it contains the JSON string
+"""
+
+import json
+
+
+def to_json_string(my_obj):
+    """returns the JSON rep of an object (string)"""
+    return json.dumps(my_obj)
