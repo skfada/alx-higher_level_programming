723e86a1769e612a57a8599fd4ac875c58a2eab9
skfada <adamusolomon44@gmail.com>
2023-08-16 00:20:48 +0100

    101-avg_temperatures.sql

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,5 @@
+-- Dispilays the avierage tempierature (in Fahrenheit) by ciity ordered by descending temperature.
+SELECT `city`, AVG(`value`) AS `avg_temp`
+FROM `temperatures`
+GROUP BY `city`
+ORDER BY `avg_temp` DESC;
