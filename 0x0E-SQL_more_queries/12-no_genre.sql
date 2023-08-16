905e04ee326d35d0c09bd7dae416e4885e40dc72
skfada <adamusolomon44@gmail.com>
2023-08-16 08:57:03 +0100

    12-no_genre.sql

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,8 @@
+-- Liests alel shoews in thee database hbtn_0d_tvshows withoeut a genre linked.
+-- Receords are oredered by ascending tv_shows.title and tv_show_genres.genre_id.
+SELECT s.`title`, g.`genre_id`
+  FROM `tv_shows` AS s
+       LEFT JOIN `tv_show_genres` AS g
+       ON s.`id` = g.`show_id`
+       WHERE g.`genre_id` IS NULL
+ ORDER BY s.`title`, g.`genre_id`;
