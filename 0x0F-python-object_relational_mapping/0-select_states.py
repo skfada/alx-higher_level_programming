20dff3199d49d7bcc7b430d726b9bcfe60b224b3
skfada <adamusolomon44@gmail.com>
2023-11-18 08:03:29 +0100

    0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -9,24 +9,19 @@ import sys
 import MySQLdb
 
 conn = MySQLdb.connect(
-    user = sys.argv[1],
-    passwd = sys.argv[2],
-    db = sys.argv[3],
-    port = 3306
+    user=sys.argv[1],
+    passwd=sys.argv[2],
+    db=sys.argv[3],
+    port=3306
 )
-
 cur = conn.cursor()
-cur.execute('\
-    SELECT * FROM states\
-    WHERE id < 6'
-)
-
+query = 'SELECT * FROM states WHERE id < 6'
+cur.execute(query)
 query_rows = cur.fetchall()
 
 if __name__ == '__main__':
     for row in query_rows:
         print(row)
-    
+
     cur.close()
     conn.close()
-    
