00b6b00ce71ebe9a512c4b7698021c5e476e0422
skfada <adamusolomon44@gmail.com>
2023-11-19 03:04:44 -0800

    3-my_safe_filter_states.py

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -5,20 +5,21 @@ table of hbtn_0e_0_usa where name matches the argument.
 But this time, write one that is safe from MySQL injections!
 """
 import MySQLdb
-from sys import argv
+import sys
 
 if __name__ == "__main__":
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=argv[1],
-        passwd=argv[2],
-        db=argv[3],
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
         charset="utf8"
         )
     cursor = db.cursor()
     cursor.execute(
-        f"SELECT * FROM states WHERE name LIKE '%{argv[4]}' ORDER BY id ASC"
+        f"SELECT * FROM states WHERE name LIKE '%{sys.argv[4]}'\
+        ORDER BY id ASC"
     )
 
     rows = cursor.fetchall()
