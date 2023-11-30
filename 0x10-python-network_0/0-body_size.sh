f1ace559fe64af97c5dfd60aed121fd930764fba
skfada <adamusolomon44@gmail.com>
2023-11-30 18:32:11 +0100

    0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 # the script sends a request to that URL displays the size of the response body
-curl -sI "$1" | grep -i Content-Length | cut -d " " -f2
+
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <URL>"
+    exit 1
+fi
+
+URL=$1
+response=$(curl -sI "$URL")
+content_length=$(echo "$response" | grep -i "Content-Length" | awk '{print $2}' | tr -d '\r\n')
+
+echo "$content_length"
