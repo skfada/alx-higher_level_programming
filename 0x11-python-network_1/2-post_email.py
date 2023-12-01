583ca857f4bf6c217cb152baa6d609044a41a0bd
skfada <adamusolomon44@gmail.com>
2023-12-01 04:35:09 -0800

    2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python3
-'''
-script that takes in a URL and an email, sends a POST request
-to the passed URL with the email as a parameter, and displays
-the body of the response (decoded in utf-8)
-'''
+"""
+POST request to the passed URL with the email as a parameter
+"""
 import urllib.request
-import sys
+from sys import argv
+
+
+def main(argv):
+    """
+    Sends a POST request to the passed URL with the email as a parameter,
+    and displays the body of the response (decoded in utf-8)
+    """
+    values = {'email': argv[2]}
+    data = urllib.parse.urlencode(values)
+    data = data.encode('utf8')
+    url = argv[1]
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        result = response.read()
+        print(result.decode('utf8'))
+
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-    email = sys.argv[2]
-    print(f"Your email is: {email}")
-
-    data = urllib.parse.urlencode({'email': email})
-    data = data.encode('utf-8')
-
-    req = urllib.request.Request(url, data=data, method='POST')
-
-    with urllib.request.urlopen(req) as response:
-        body = response.read().decode('utf-8')
-        print(body)
+    main(argv)
