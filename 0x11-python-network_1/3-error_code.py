88cd143be4abee3ced392b1748394055510e9634
skfada <adamusolomon44@gmail.com>
2023-12-01 04:26:59 -0800

    3-error_code.py

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''
+script that takes in a URL, sends a request to the URL
+and displays the body of the response (decoded in utf-8).
+'''
+import urllib.request
+import urllib.error
+import sys
+
+def fetch_url(url):
+    try:
+        with urllib.request.urlopen(url) as response:
+            body = response.read().decode('utf-8')
+            print(body)
+    except urllib.error.HTTPError as e:
+        print(f"Error code: {e.code}")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python script_name.py <URL>")
+        sys.exit(1)
+
+    url = sys.argv[1]
+    fetch_url(url)
