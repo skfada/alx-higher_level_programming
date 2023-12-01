ea33dead35a06ede8bba7d2e347c4ad4dd5623c2
skfada <adamusolomon44@gmail.com>
2023-12-01 05:31:36 +0100

    1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# this script sends a GET req to URL and display response body
-curl -Ls "$1"
+# send a GET request to an URL with curl, and display the body of the response
+curl -sL "$1"
