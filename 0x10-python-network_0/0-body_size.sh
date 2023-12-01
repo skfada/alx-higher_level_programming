c1ddfb7d2f1848c8741fae5f528d3386eae462ef
skfada <adamusolomon44@gmail.com>
2023-12-01 05:31:34 +0100

    0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
-# the script sends a request to that URL displays the size of the response body
-
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-URL=$1
-response=$(curl -sI "$URL")
-content_length=$(echo "$response" | grep -i "Content-Length" | awk '{print $2}' | tr -d '\r\n')
-
-echo "$content_length"
+# Get the comtent-lenght of a giving ip address
+curl -sI "$1" | awk '/Content-Length/{print $2}'
