f7b59a045c4ba6d1b9d50185b580a2ab0c96eeb0
skfada <adamusolomon44@gmail.com>
2024-01-12 16:42:36 +0100

    0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,6 @@
 #!/usr/bin/node
-
+// reads and prints the content of a file
 const fs = require('fs');
-const filename = process.argv[2];
-
-fs.readFile(filename, 'utf-8', (error, content) => {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log(content);
-  }
+fs.readFile(process.argv[2], 'utf8', function (err, data) {
+  if (err) { console.log(err); } else { process.stdout.write(data); }
 });
